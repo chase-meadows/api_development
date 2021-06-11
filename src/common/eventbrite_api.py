0c3b4a240a41c2c06615f84f6e03a6dec836a6b9
Chase Meadows <chase.meadows22@gmail.com>
2021-06-11 00:42:58 -0400

    implement simple request wrapper

--- src/common/eventbrite_api.py
+++ src/common/eventbrite_api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from dotenv import dotenv_values
 
 
@@ -15,8 +16,9 @@ class EventBriteApi(object):
         
         
     def _get_request_wrapper(self,
+                             object_type: str,
                              url: str,
-                             data: dict):
+                             **kwargs: dict):
         # wrapper for API calls to handle pagination
         headers = {
                 'Authorization': 'Bearer ' + self.private_token
@@ -24,22 +26,66 @@ class EventBriteApi(object):
         
         response = requests.get(url     = url,
                                 headers = headers,
-                                data    = data)
+                                **kwargs)
         
         if response.status_code == 200:
-            # figure out how we want to handle response codes and failures
-            return response
+            # Load Response data into dict
+            data = json.loads(response.content)
+            
+            # Handle Pagination
+            pagination = data['pagination']
+            
+            if not pagination['has_more_items']:
+                # If no additional requests are required
+                return data[object_type]
+            else:
+                # If we need to handle pagination
+                current_page = pagination['page_number']
+                total_pages = pagination['page_count']
+                # Create list to bind results
+                output_list = []
+                while pagination['has_more_items']:
+                    # handle requesting and re-requesting
+                    # for multiple pages
+                    continued_url = url + '?continuation=' + pagination['continuation']
+                    
+                    # perform the request again using the continuation url
+                    response = requests.get(url     = continued_url,
+                                            headers = headers,
+                                            **kwargs)
+                    
+                    data = json.loads(response.content)
+                    
+                    pagination = data['pagination']
+                    output_list.append(data[object_type])
+                
+                return output_list
         else:
-            return response
+            # If response status code is not 200
+            return None
         
-        def list_my_organizations(self):
+
+        
+    def list_my_organizations(self):
             # method to list organizations a user has access to
             url = self._base_url + '/users/me/organizations/'
             
-            response = self._get_request_wrapper(url = url,
-                                                 data = data)
+            response = self._get_request_wrapper(object_type = 'organizations',
+                                                 url = url)
             
             return response
+        
+    def get_venue(self,
+                  venue_id: str
+                  ) -> dict:
+        # method to get venue by id
+        url = self._base_url + f'/venues/{venue_id}/'
+        
+        response = self._get_request_wrapper(object_type = 'venues',
+                                             url = url)
+            
+        return response
+
         
 
 my_api = EventBriteApi()
